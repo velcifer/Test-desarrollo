737ceb9347e563a6546ba9ce705f6ef25e98942a
velcifer <50281349+velcifer@users.noreply.github.com>
2021-03-28 19:53:50 -0500

    Create Bootstrap.js

---
+++ Bootstrap.js
@@ -0,0 +1,8 @@
+//es un framework CSS y Javascript diseñado para la creación de interfaces limpias y con un diseño responsive. Además, ofrece un amplio abanico de herramientas y funciones,
+//de manera que los usuarios pueden crear prácticamente cualquier tipo de sitio web haciendo uso de los mismos.
+
+Ejemplo de Bootstrap 4 y este js es es que se coloca en mi proyecto para poder trabajar con css y javascript:
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
