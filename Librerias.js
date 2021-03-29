37603b47167ff1d55da01e2e94f27592889a9348
velcifer <50281349+velcifer@users.noreply.github.com>
2021-03-28 19:03:38 -0500

    Create Librerias.js

---
+++ Librerias.js
@@ -0,0 +1,78 @@
+// Hay muchas librerias de Javascript pero yo en especial uso doce dependiendo el proyecto y son://
+
+1) jQuery:
+
+Es una libreria de javascript rapida e incluida en un solo archivo y sirve para manipular el Dom y 
+mostrar intareaciones, y cambios en la web. 
+
+2) Moment.js :
+
+Te ayuda a trabajar con fechas Lo que nos permite, por ejemplo, es que en vez de mostrar la fecha en formato “publicado el 07 de julio del 2020 a las 7:50 a.m.”, 
+como lo devuelve JavaScript, Moment.js lo estandariza y simplifica poniendo “hace 5 minutos”. Nos da la posibilidad de poner varios formatos de fechas.
+
+moment().format("DD-MM-YYYY");                 //"19-06-2016"
+moment().format('MMMM Do YYYY, h:mm:ss a');    //"junio 19º 2016, 7:36:05 pm"
+moment().format('dddd');                       //"domingo"
+moment().format("MMM Do YY");                  //"jun. 19º 16"
+moment().format('dddd [cualquier cosa] YYYY'); //"domingo cualquier cosa 2016"
+
+3) anime.js
+
+Se trata de una librería para animar diferentes propiedades CSS, SVG o atributos DOM en una página web. Permite controlar todos los aspectos de la animación y
+proporciona muchas maneras de especificar las propiedades que quieres animar  o los elementos que quieres usar.
+
+Ejemplo: https://animejs.com/
+
+4) Ramda :
+
+Ramda es una librería funcional y práctica para programadores de JavaScript, Las funciones de Ramda se ejecutan automáticamente, 
+lo que permite crear nuevas funciones a partir de las antiguas simplemente al no proporcionar los parámetros finales.
+
+5) D3.js :
+
+Es una librería de JavaScript para manipular documentos basados ​​en datos. Te ayuda a proporciona una gran facilidad y
+flexibilidad para crear estas visualizaciones de datos, infogramas dinámicos e interactivos en navegadores web.
+
+Ejemplo:  https://d3js.org/
+
+6) Chart.js:
+
+Chart.js es una librería sencilla y muy fácil de utilizar que te permite incluir gráficos animados e interactivos en un sitio web.
+
+Ejemplo: https://www.chartjs.org/
+
+7) MathJS :
+
+es una extensa librería matemática para JavaScript muy sencilla de utilizar. Cuenta con un analizador de expresiones flexible y con soporte para cálculo simbólico, 
+viene con un amplio conjunto de funciones y constantes incorporadas, y ofrece una solución integrada para trabajar con diferentes tipos de datos.
+
+8) Hammer.js :
+
+Se puede usar para crear aplicaciones web que requieren realizar acciones como desplazarse, deslizar, rotar y hacer zoom en los gestos táctiles.
+
+Ejemplo: https://hammerjs.github.io/
+
+9) React:
+
+Ayuda a crear interfaces de usuario interactivas de forma sencilla. A través de esta librería podrás diseñar vistas simples para cada estado en la aplicación. 
+React se encargará de actualizar y renderizar de manera eficiente los componentes correctos cuando los datos cambien.
+
+Uno de mis trabajos o ejemplos: https://react-firebase-login-template.web.app/
+
+10) Redux:
+
+Con esta librería podrás emitir actualizaciones de estado en respuesta a acciones. 
+Estas modificaciones podrás hacerlas a través de objetos sencillos o acciones, y no a través de cambios directos en el estado.
+
+11) Glimmer.js:
+
+Glimmer.js proporciona funcionalidad para definir, componer y representar componentes y mantiene el DOM sincronizado con el estado interno del árbol de componentes.
+
+Ejemplo: https://glimmerjs.com/
+
+12) Codemirror:
+
+Codemirror nos permite insertar  un editor de código en nuestro proyecto para escribir código en el navegador.
+
+Ejemplo: https://codemirror.net/
+
